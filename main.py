7c8e3fa70dc7fba3e58f3e2d86f787881bc8cf71
meshu-dev <harmeshuppal@gmail.com>
2023-04-20 18:12:16 +0100

    Add tag routes

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
-from routers import index, notes
+from routers import index, notes, tags
 
 app = FastAPI()
 
-app.include_router(notes.router)
 app.include_router(index.router)
+app.include_router(notes.router)
+app.include_router(tags.router)
